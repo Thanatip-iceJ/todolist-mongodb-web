ed3b9b65a6e90122326901c0b74306382087883a
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-12-13 00:58:41 +0700

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-xl text-red-500">Hello, World!!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
